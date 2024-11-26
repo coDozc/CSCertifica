0cce1940baefe9bd4549cc31bf6c4a44a82aa39b
coDozc <ozcan_bayram57@hotmail.com>
2024-11-26 17:50:10 +0300

    01 basics are added

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+            //Console.WriteLine("Merhaba");
+            //Console.Write("Satırı aşağı indirmez.");
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("*** Yemek Kategorileri ***");
+            //Console.WriteLine();
+            //Console.WriteLine("1- Çorbalar");
+            //Console.WriteLine("2- Ana Yemekler");
+            //Console.WriteLine("3- Salatalar");
+            //Console.WriteLine("4- Tatlılar");
+            //Console.WriteLine();
+            //Console.WriteLine("*** Yemek Kategorileri ***");
+            #endregion
+
+            #region String Değişkenler
+            //string name;
+            //name = "Bayram";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Ece";
+            //customerSurname = "Seçkin";
+            //customerPhone = "+90 533 694 230 14";
+            //customerEmail = "ecsckn@gmail.com";
+            //district = "Bakırköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("***** Rezervasyon Kartı *****");
+            //Console.WriteLine();
+            //Console.WriteLine("-----------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + "/" + city);
+            //Console.WriteLine("-----------------------------------");
+
+            #endregion
+
+            #region int değişkenler
+            int num = 7;
+            Console.WriteLine(num);
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
